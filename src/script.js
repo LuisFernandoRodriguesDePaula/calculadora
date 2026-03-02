e8ae9ee57d64800160f9d3bc55bd24bdd5a99161
Luis Fernando <luisfernandorodriguesdepaula@gmail.com>
2026-03-02 20:58:04 -0300

    ajuste de pastas

--- src/script.js
+++ src/script.js
@@ -1,15 +1,38 @@
-const display = document.querySelector(".display");
+const display = document.querySelector("#display");
 const button = document.querySelector(".botoes");
+let ultimoValor = "";
 
 function addValue(value){
-    display.value = "";
+    display.value += value;
+    ultimoValor += value;
+}
+
+function operador(op){
+    if(display.value.indexOf(op) < 0){
+        display.value += op;
+        ultimoValor = "";
+    }
 }
 
 function clearDisplay(){
     display.value = "";
 }
 
+function apaga(){
+    let valor = display.value;
+    valor = valor.substring(0, valor.length - 1);
+    display.value = valor;
+    ultimoValor = ultimoValor.substring(0, ultimoValor.length -1)
+}
+
+function negativo(){
+    let parteSemUltimoValor = display.value.substring(0, display.value.length - ultimoValor.length);
+    let valor = Number(ultimoValor) * -1;
+    display.value = parteSemUltimoValor + valor;
+    ultimoValor = valor.toString();
+}
+
+
 function calculate(){
     display.value = eval(display.value);
 }
-
